01d73e2655ec4b3f1e36f023c4a0332aaefe1d21
Timon Käch <78795905+CyberTimon@users.noreply.github.com>
2023-03-12 11:30:02 +0100

    Auto install dependencies in bat file. Thanks to @Nitram_02

--- start_app.bat
+++ start_app.bat
@@ -1,1 +1,10 @@
+@echo off
+
+pip show flask python-pptx flask_limiter openai regex collection >nul 2>&1
+if %errorlevel% == 0 (goto end) 
+echo Installing dependencies
+
+pip install flask python-pptx flask_limiter openai regex collection
+
+:end
 python app.py 
